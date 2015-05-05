c396a3aa895fc6943c5d157d37eb39e58f6f6da9
Ben Buckman <ben.buckman@goodeggs.com>
2015-05-04 17:16:40 -0700

    alternate npm-install.sh for ops.
    (was originally on an 'ops' branch, but ecru has its `travis` fn hard-coded, so that didn't work.)

---
+++ ops-npm-install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -ex
+
+node -v
+npm -v
+
+# travis_retry isn't available to sub-scripts
+retry () { for i in 1 2 3; do "$@" && return || sleep 10; done; exit 1; }
+
+npm cache clean
+
+if test -f ./node_modules/.node-version && [ $(cat ./node_modules/.node-version) != `node -v` ]; then
+  echo "Node version changed since last build; rebuilding dependencies"
+  npm rebuild
+fi
+
+retry npm install
+
+echo `node -v` > ./node_modules/.node-version
+
+npm ls || true   # this fails on extraneous modules, ignore.
