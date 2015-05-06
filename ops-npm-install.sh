2a7f28c9ca0e4d3ba5f8be6ce9342b22d7e39533
Ben Buckman <ben.buckman@goodeggs.com>
2015-05-05 17:35:01 -0700

    tweak ops-npm-install.sh some more.

--- ops-npm-install.sh
+++ ops-npm-install.sh
@@ -13,13 +13,14 @@ retry () { for i in 1 2 3; do "$@" && return || sleep 10; done; exit 1; }
 # - with only `npm install`, modules removed in shrinkwrap don't get removed from app.
 #   `npm install` also does not necessarily update all submodules per shrinkwrap.
 # - with `npm prune`, sometimes modules that should be kept, are removed, don't know why.
-# - `npn update` in theory makes sure all missing modules are installed,
+# - `npm update` in theory makes sure all missing modules are installed,
 #   hopefully filling any gaps left from `install` and `prune`.
+# - but `npm update` doesn't run aperture (for symlinks)... oy.
 #
 npm cache clean
-retry npm install
 npm prune
-npm update
+retry npm install
+retry npm update
 
 if test -f ./node_modules/.node-version && [ $(cat ./node_modules/.node-version) != `node -v` ]; then
   echo "Node version changed since last build; rebuilding dependencies"
