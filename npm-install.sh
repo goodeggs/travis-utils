a4d47645190ec6bf8dfba8b7de2741ca3e48e31a
Ben Buckman <ben.buckman@goodeggs.com>
2015-05-05 17:12:27 -0700

    more aggressive npm-install script for ops apps?

--- npm-install.sh
+++ npm-install.sh
@@ -4,15 +4,24 @@ set -ex
 # travis_retry isn't available to sub-scripts
 retry () { for i in 1 2 3; do "$@" && return || sleep 10; done; exit 1; }
 
-npm prune
+#
+# [bb 5/5/15] - WIP:
+# - the caches on ecru and travis are really dumb, just whole saved directories.
+# - with only `npm install`, modules removed in shrinkwrap don't get removed from app.
+#   `npm install` also does not necessarily update all submodules per shrinkwrap.
+# - with `npm prune`, sometimes modules that should be kept, are removed, don't know why.
+# - `npn update` in theory makes sure all missing modules are installed,
+#   hopefully filling any gaps left from `install` and `prune`.
+#
 npm cache clean
+retry npm install
+npm prune
+npm update
 
 if test -f ./node_modules/.node-version && [ $(cat ./node_modules/.node-version) != `node -v` ]; then
   echo "Node version changed since last build; rebuilding dependencies"
   npm rebuild
 fi
-
-retry npm install
 
 echo `node -v` > ./node_modules/.node-version
 
