89aa8b23a54d797bee9cdafac0cc5ed0d4ebb1ff
Ben Buckman <ben.buckman@goodeggs.com>
2015-05-06 10:37:17 -0700

    try force-wiping node_modules dir

--- ops-npm-install.sh
+++ ops-npm-install.sh
@@ -10,20 +10,9 @@ npm cache ls | wc -l   # dumb count. (too many lines to fully dump)
 # travis_retry isn't available to sub-scripts
 retry () { for i in 1 2 3; do "$@" && return || sleep 10; done; exit 1; }
 
-#
-# [bb 5/5/15] - WIP:
-# - the caches on ecru and travis are really dumb, just whole saved directories.
-# - with only `npm install`, modules removed in shrinkwrap don't get removed from app.
-#   `npm install` also does not necessarily update all submodules per shrinkwrap.
-# - with `npm prune`, sometimes modules that should be kept, are removed, don't know why.
-# - `npm update` in theory makes sure all missing modules are installed,
-#   hopefully filling any gaps left from `install` and `prune`.
-# - but `npm update` doesn't run aperture (for symlinks)... oy.
-#
-#retry npm cache clean
-#retry npm prune
+ls node_modules/
+rm -rf node_modules
 retry npm install
-#retry npm update
 
 if test -f ./node_modules/.node-version && [ $(cat ./node_modules/.node-version) != `node -v` ]; then
   echo "Node version changed since last build; rebuilding dependencies"
