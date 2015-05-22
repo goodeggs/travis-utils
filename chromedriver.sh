16a0f6b9025deab9fb410e9b314b83d1ba8da391
Bob Zoller <bob@zoller.us>
2015-05-22 10:01:54 -0700

    revert to chromedriver 2.10

--- chromedriver.sh
+++ chromedriver.sh
@@ -2,7 +2,7 @@
 set -ex
 
 CHROMEDRIVER=chromedriver_linux64.zip
-curl -Lo $CHROMEDRIVER http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION:-2.15}/$CHROMEDRIVER
+curl -Lo $CHROMEDRIVER http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION:-2.10}/$CHROMEDRIVER
 unzip $CHROMEDRIVER
 
 export DISPLAY=:99.0
