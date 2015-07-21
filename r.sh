d6f7b946105c303a8cd0521a35033d5a463f78a7
Bob Zoller <bob@zoller.us>
2015-07-21 01:21:59 -0700

    r.sh specifies precise packages

--- r.sh
+++ r.sh
@@ -8,7 +8,7 @@ else
 fi
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-echo 'deb http://cran.cnr.berkeley.edu/bin/linux/ubuntu trusty/' | sudo tee /etc/apt/sources.list.d/r.list
+echo 'deb http://cran.cnr.berkeley.edu/bin/linux/ubuntu precise/' | sudo tee /etc/apt/sources.list.d/r.list
 sudo apt-get update
 sudo apt-get install $R_PACKAGE
 
