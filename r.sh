1d254b5ec77b77e589684b5df137c0a1235373ff
Bob Zoller <bob@zoller.us>
2015-10-19 16:33:05 -0700

    use ppa:marutter/rrutter PPA for R packages

--- r.sh
+++ r.sh
@@ -8,7 +8,7 @@ else
 fi
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-echo 'deb http://cran.cnr.berkeley.edu/bin/linux/ubuntu precise/' | sudo tee /etc/apt/sources.list.d/r.list
+sudo add-apt-repository ppa:marutter/rrutter
 sudo apt-get update
 sudo apt-get install $R_PACKAGE
 
