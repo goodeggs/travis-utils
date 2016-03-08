1284dfb1fb86a1564de55e42ccfebf584dec7766
Bob Zoller <bob@zoller.us>
2016-03-07 18:51:59 -0800

    ignore apt update errors
    workaround https://github.com/travis-ci/travis-ci/issues/5221

--- mongodb.sh
+++ mongodb.sh
@@ -9,6 +9,6 @@ fi
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
+sudo apt-get update || true
 sudo apt-get install $MONGO_PACKAGE
 
