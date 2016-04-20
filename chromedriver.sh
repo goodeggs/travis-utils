9d68ba3da936b5833091ff2b1a17131e3765b65c
Bob Zoller <bob@zoller.us>
2016-04-20 15:38:31 -0700

    Revert "Merge pull request #5 from goodeggs/move-xvfb"
    This reverts commit fa2d343c2fdb21abae129b57dbf8a40dc715e717, reversing
    changes made to aaeee64d32aa863998db86a4941bc9d20a4873d2.

--- chromedriver.sh
+++ chromedriver.sh
@@ -5,4 +5,6 @@ CHROMEDRIVER=chromedriver_linux64.zip
 curl -Lo $CHROMEDRIVER http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION:-2.21}/$CHROMEDRIVER
 unzip $CHROMEDRIVER
 
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
 ./chromedriver "$@" &
