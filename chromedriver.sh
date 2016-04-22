1be31f63d0d34973a42c3b198c5212c200147ef2
Bob Zoller <bob@zoller.us>
2016-04-22 14:26:18 -0700

    start xvfb in both places

--- chromedriver.sh
+++ chromedriver.sh
@@ -6,5 +6,6 @@ curl -Lo $CHROMEDRIVER http://chromedriver.storage.googleapis.com/${CHROMEDRIVER
 unzip $CHROMEDRIVER
 
 export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
+/etc/init.d/xvfb start
+
 ./chromedriver "$@" &
