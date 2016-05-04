e7d15187e72ddcce041981d142c9d9e728ad5736
Bob Zoller <bob@zoller.us>
2016-05-04 15:05:06 -0700

    try mongodb binary tarballs

--- mongodb.sh
+++ mongodb.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 set -ex
 
-if [ "$MONGO_VERSION" ]; then
-  MONGO_PACKAGE="mongodb-org=${MONGO_VERSION}"
-else
-  MONGO_PACKAGE="mongodb-org"
-fi
+mkdir -p /tmp/mongodb/data
+cd /tmp/mongodb
+curl -Lo mongodb.tgz http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGO_VERSION}.tgz
+tar -xvf mongodb.tgz
+/tmp/mongodb/mongodb-linux-x86_64-${MONGO_VERSION}/bin/mongod --dbpath /tmp/mongodb/data --bind_ip 127.0.0.1 --auth &> /dev/null &
 
 # appears mongodb killed this key on 2016/05/23
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-curl https://www.mongodb.org/static/pgp/server-2.6.pub | sudo apt-key add -
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update || true
-sudo apt-get -y install $MONGO_PACKAGE
-
+#echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+#sudo apt-get update || true
+#sudo apt-get -y install $MONGO_PACKAGE
