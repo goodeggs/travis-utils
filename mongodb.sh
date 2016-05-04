c9def0513daa35cba3958d6469a83b3b09899e23
Bob Zoller <bob@zoller.us>
2016-05-04 14:23:34 -0700

    try to trust an alternate signing key for mongodb
    their GPG key went missing from keyserver.ubuntu.com?

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,9 @@ else
   MONGO_PACKAGE="mongodb-org"
 fi
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+# appears mongodb killed this key on 2016/05/23
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+curl https://www.mongodb.org/static/pgp/server-2.6.pub | sudo apt-key add -
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update || true
 sudo apt-get -y install $MONGO_PACKAGE
