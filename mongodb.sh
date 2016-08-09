286b21281b483996b126a66c4e8802144b0d8b5d
Bob Zoller <bob@zoller.us>
2016-08-09 11:36:43 -1000

    echo mongo logs to debug

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ mkdir -p /tmp/mongodb/data
 cd /tmp/mongodb
 curl -Lo mongodb.tgz http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGO_VERSION}.tgz
 tar -xvf mongodb.tgz
-/tmp/mongodb/mongodb-linux-x86_64-${MONGO_VERSION}/bin/mongod --dbpath /tmp/mongodb/data --bind_ip 127.0.0.1 --auth > /dev/null 2>&1 &
+/tmp/mongodb/mongodb-linux-x86_64-${MONGO_VERSION}/bin/mongod --dbpath /tmp/mongodb/data --bind_ip 127.0.0.1 --auth &
 
 # appears mongodb killed this key on 2016/05/23
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
