208c02445d7d6014f37f0cddd03d09973bea0c61
Bob Zoller <bob@zoller.us>
2016-08-09 11:49:25 -1000

    stop the mongodb service if it's running

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,8 @@ mkdir -p /tmp/mongodb/data
 cd /tmp/mongodb
 curl -Lo mongodb.tgz http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGO_VERSION}.tgz
 tar -xvf mongodb.tgz
-/tmp/mongodb/mongodb-linux-x86_64-${MONGO_VERSION}/bin/mongod --dbpath /tmp/mongodb/data --bind_ip 127.0.0.1 --auth &
+sudo service mongodb stop || true # stop the mongodb service if it's running so we can bind
+/tmp/mongodb/mongodb-linux-x86_64-${MONGO_VERSION}/bin/mongod --dbpath /tmp/mongodb/data --bind_ip 127.0.0.1 --auth > /dev/null 2>&1 &
 
 # appears mongodb killed this key on 2016/05/23
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
