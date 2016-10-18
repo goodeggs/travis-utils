a9af39314d9e149d1334a5aee5142f6501be2570
Bob Zoller <bob@zoller.us>
2016-10-17 15:31:07 -1000

    chromedriver 2.24

--- chromedriver.sh
+++ chromedriver.sh
@@ -2,7 +2,7 @@
 set -ex
 
 CHROMEDRIVER=chromedriver_linux64.zip
-curl -Lo $CHROMEDRIVER http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION:-2.21}/$CHROMEDRIVER
+curl -Lo $CHROMEDRIVER http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION:-2.24}/$CHROMEDRIVER
 unzip $CHROMEDRIVER
 
 export DISPLAY=:99.0
