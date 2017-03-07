2b9a6660e4da5ee69a34d5387a9955ad28783ca9
Bob Zoller <bob@zoller.us>
2017-03-07 13:48:26 -1000

    C

--- docker-compose.sh
+++ docker-compose.sh
@@ -6,7 +6,7 @@ if [ -z "$CACHE_DIR" ]; then
   CACHE_DIR="~/.docker-compose-cacher"
 fi
 
-curl -sSL https://github.com/goodeggs/docker-compose-cacher/releases/download/v1.2.0/docker-compose-cacher_v1.2.0_linux_amd64.tar.gz | tar xz -c /tmp docker-compose-cacher
+curl -sSL https://github.com/goodeggs/docker-compose-cacher/releases/download/v1.2.0/docker-compose-cacher_v1.2.0_linux_amd64.tar.gz | tar xz -C /tmp docker-compose-cacher
 
 /tmp/docker-compose-cacher -d "$CACHE_DIR"
 docker-compose up -d
