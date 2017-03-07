d066281351c90dc370558c9a928f6d01058351e0
Bob Zoller <bob@zoller.us>
2017-03-07 13:25:33 -1000

    allow setting of CACHE_DIR

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,8 +3,13 @@ set -e
 set -o pipefail
 # set -x
 
+if [ -z "$CACHE_DIR" ]; then
+  CACHE_DIR="~/.docker-compose-cacher"
+fi
+exit 1
+
 curl -sSL https://github.com/goodeggs/docker-compose-cacher/releases/download/v1.2.0/docker-compose-cacher_v1.2.0_linux_amd64.tar.gz | tar xz -c /tmp docker-compose-cacher
 
-/tmp/docker-compose-cacher
+/tmp/docker-compose-cacher -d "$CACHE_DIR"
 docker-compose up -d
 
