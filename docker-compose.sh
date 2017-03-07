f55bab86b9097eb2b96c7901083d66510678b963
Bob Zoller <bob@zoller.us>
2017-03-07 13:19:14 -1000

    add docker-compose.sh helper

---
+++ docker-compose.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -o pipefail
+# set -x
+
+curl -sSL https://github.com/goodeggs/docker-compose-cacher/releases/download/v1.2.0/docker-compose-cacher_v1.2.0_linux_amd64.tar.gz | tar xz -c /tmp docker-compose-cacher
+
+/tmp/docker-compose-cacher
+docker-compose up -d
+
