f654a0ff589eb6dc930aa1d63603996bdcb41ab2
Bob Zoller <bob@zoller.us>
2017-03-07 13:37:11 -1000

    remove exit 1

--- docker-compose.sh
+++ docker-compose.sh
@@ -5,7 +5,6 @@ set -e
 if [ -z "$CACHE_DIR" ]; then
   CACHE_DIR="~/.docker-compose-cacher"
 fi
-exit 1
 
 curl -sSL https://github.com/goodeggs/docker-compose-cacher/releases/download/v1.2.0/docker-compose-cacher_v1.2.0_linux_amd64.tar.gz | tar xz -c /tmp docker-compose-cacher
 
