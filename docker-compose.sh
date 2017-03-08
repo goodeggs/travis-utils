619c3855bbaab2fb7cfe2f349e9224d1cf914b2b
Bob Zoller <bob@zoller.us>
2017-03-07 14:01:23 -1000

    mktemp

--- docker-compose.sh
+++ docker-compose.sh
@@ -6,8 +6,10 @@ if [ -z "$CACHE_DIR" ]; then
   CACHE_DIR="~/.docker-compose-cacher"
 fi
 
-curl -sSL https://github.com/goodeggs/docker-compose-cacher/releases/download/v1.2.0/docker-compose-cacher_v1.2.0_linux_amd64.tar.gz | tar xz -C /tmp docker-compose-cacher
+dcc=$(mktemp -d)
 
-/tmp/docker-compose-cacher -d "$CACHE_DIR"
+curl -sSL https://github.com/goodeggs/docker-compose-cacher/releases/download/v1.2.0/docker-compose-cacher_v1.2.0_linux_amd64.tar.gz | tar xz -C "$dcc" docker-compose-cacher
+
+"$dcc/docker-compose-cacher" -d "$CACHE_DIR"
 docker-compose up -d
 
