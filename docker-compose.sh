48f565e054ce5ddd9e1016b7d5e5772c20bdf261
Bob Zoller <bob@zoller.us>
2017-03-13 14:23:19 -1000

    instrument docker-compose timeouts

--- docker-compose.sh
+++ docker-compose.sh
@@ -11,12 +11,13 @@ start=$(date "+%s")
 
 caching=$(grep -m1 -ao '[0-1]' /dev/urandom | head -n1)
 if [ "$caching" = "0" ]; then
-  echo "skipping docker-compose-cacher"
+  echo "using docker-compose pull"
+  docker-compose pull
 else
   echo "using docker-compose-cacher"
 
   if [ -z "$CACHE_DIR" ]; then
-    CACHE_DIR="~/.docker-compose-cacher"
+    CACHE_DIR="$HOME/.docker-compose-cacher"
   fi
 
   dcc=$(mktemp -d)
@@ -26,10 +27,14 @@ else
   "$dcc/docker-compose-cacher" -d "$CACHE_DIR"
 fi
 
-docker-compose up -d
-
 end=$(date "+%s")
-duration=$(( $end - $start ))
-
+duration=$(( end - start ))
 curl -sS -XPOST -d "docker-compose-cacher experiment results caching=${caching} duration=${duration}" "$SUMO_ENDPOINT"
+
+timeout 1m docker-compose up -d
+if [ "$?" -eq 124 ]; then
+  echo "docker-compose timed out... killing build."
+  curl -sS -XPOST -d "docker-compose timeout" "$SUMO_ENDPOINT"
+  exit 1
+fi
 
