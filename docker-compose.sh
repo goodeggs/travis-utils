083cfd54785f3ace2e15e444adc8e43db6cfcf4c
Bob Zoller <bob@zoller.us>
2017-03-14 13:55:53 -1000

    cache docker-compose-cacher binary

--- docker-compose.sh
+++ docker-compose.sh
@@ -12,6 +12,7 @@ start=$(date "+%s")
 caching=$(grep -m1 -ao '[0-1]' /dev/urandom | head -n1)
 if [ "$caching" = "0" ]; then
   echo "using docker-compose pull"
+  full_cache="-1"
   docker-compose pull
 else
   echo "using docker-compose-cacher"
@@ -20,16 +21,21 @@ else
     CACHE_DIR="$HOME/.docker-compose-cacher"
   fi
 
-  dcc=$(mktemp -d)
+  mkdir -p "$CACHE_DIR"
+
+  if [ ! -x "$CACHE_DIR/docker-compose-cacher" ] || [ "$(shasum -a 256 "$CACHE_DIR/docker-compose-cacher")" != "8aae2d21b846afab92f03df535bf91b22579e7d796936fc1031d236a95f94871" ]; then
+    full_cache="0"
+    curl -sSL https://github.com/goodeggs/docker-compose-cacher/releases/download/v1.3.0/docker-compose-cacher_v1.3.0_linux_amd64.tar.gz | tar xz -C "$CACHE_DIR" docker-compose-cacher
+  else
+    full_cache="1"
+  fi
   
-  curl -sSL https://github.com/goodeggs/docker-compose-cacher/releases/download/v1.3.0/docker-compose-cacher_v1.3.0_linux_amd64.tar.gz | tar xz -C "$dcc" docker-compose-cacher
-  
-  "$dcc/docker-compose-cacher" -d "$CACHE_DIR"
+  "$CACHE_DIR/docker-compose-cacher" -d "$CACHE_DIR"
 fi
 
 end=$(date "+%s")
 duration=$(( end - start ))
-curl -sS -XPOST -d "docker-compose-cacher experiment results caching=${caching} duration=${duration}" "$SUMO_ENDPOINT"
+curl -sS -XPOST -d "docker-compose-cacher experiment results caching=${caching} full_cache=${full_cache} duration=${duration}" "$SUMO_ENDPOINT"
 
 timeout 1m docker-compose up -d
 if [ "$?" -eq 124 ]; then
