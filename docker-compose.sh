940e1fd4673b7543ba928300bb80cf2a05d1e7a4
Bob Zoller <bob@zoller.us>
2017-03-22 11:41:12 -1000

    remove docker-compose-cacher experiment; add --parallel

--- docker-compose.sh
+++ docker-compose.sh
@@ -7,31 +7,34 @@ if [ -z "$SUMO_ENDPOINT" ]; then
   exit 1
 fi
 
-start=$(date "+%s")
+debug() {
+  echo "+$*"
+  "$@"
+}
 
-caching=$(grep -m1 -ao '[0-1]' /dev/urandom | head -n1)
-if [ "$caching" = "0" ]; then
-  echo "using docker-compose pull"
-  docker-compose pull
+total_start=$(date "+%s")
+repo=$(basename "$(git remote get-url origin)" .git)
+
+pull_start=$(date "+%s")
+if docker-compose help pull | grep -q -- "--parallel"; then
+  debug docker-compose pull --parallel
 else
-  echo "using docker-compose-cacher"
-  if [ -z "$CACHE_DIR" ]; then
-    CACHE_DIR="$HOME/.docker-compose-cacher"
-  fi
-  mkdir -p "$CACHE_DIR"
-  docker-compose-cacher -d "$CACHE_DIR"
+  debug docker-compose pull
 fi
+pull_end=$(date "+%s")
+pull_t=$(( pull_end - pull_start ))
 
-end=$(date "+%s")
-duration=$(( end - start ))
-curl -sS -XPOST -d "docker-compose-cacher experiment results caching=${caching} duration=${duration}" "$SUMO_ENDPOINT"
+up_start=$(date "+%s")
+debug docker-compose up -d
+up_end=$(date "+%s")
+up_t=$(( up_end - up_start ))
 
-timeout 1m docker-compose up -d
-if [ "$?" -eq 124 ]; then
-  echo "docker-compose timed out... killing build."
-  curl -sS -XPOST -d "docker-compose timeout" "$SUMO_ENDPOINT"
-  exit 1
-fi
+total_end=$(date "+%s")
+total_t=$(( total_end - total_start ))
 
-echo "docker-compose is up"
+msg=$(cat <<-EOF
+{"msg":"ci timing","module":"docker-compose","repo":"${repo}","pull":${pull_t},"up":${up_t},"total":${total_t}}
+EOF
+)
+curl -sS -m10 -XPOST -d "$msg" "$SUMO_ENDPOINT" || true
 
